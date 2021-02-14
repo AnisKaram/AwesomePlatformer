24672a08694cab0f4ce2307814e12db47efe5450
Anis <anisTkaram@hotmail.com>
2021-02-14 19:38:55 +0200

    Adding UI, Level, Follow camera Script
    Adding Level to the game with UI text and image + follow camera script

---
+++ Assets/Scripts/BG Script/BackgroundScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // SpriteRenderer to give us the background in unity world space
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+
+        transform.localScale = new Vector3 (1, 1, 1);
+
+        // Give us the width of the backgorund we already assigned
+        float width = sr.sprite.bounds.size.x;
+
+        // Give us the height of the backgorund we already assigned
+        float height = sr.sprite.bounds.size.y;
+
+        // height is = Orthograpgic size, which is half of the size (size if 10, orthographic size is 5 * 2 = 10)
+        // Camera.main : will refer to the main camera that is tagged with "MainCamera"
+        float worldScreenHeight = Camera.main.orthographicSize * 2f;
+
+        // Screen height & width is the actual size in game (1280 * 720) and we will divide it by the orthographic size
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        Vector3 tempScale = transform.localScale;
+        tempScale.x = worldScreenWidth / width + 0.3f;
+        tempScale.y = worldScreenHeight / height + 0.3f;
+
+        transform.localScale = tempScale;
+    }
+}
---
+++ Assets/Scripts/Enemy Scripts/FrogScript.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogScript : MonoBehaviour
+{
+
+    private Animator anim;
+
+    private bool animation_Started;
+    private bool animation_Finished;
+
+    private int timesJumped;
+    private bool jumpLeft = true;
+
+    private string coroutineName = "FrogJump";
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine (coroutineName);
+    }
+
+    // LateUpdate is called after the frame is finished
+    void LateUpdate()
+    {
+        if (animation_Finished && animation_Started)
+        {
+            animation_Started = false;
+
+            // These 2 line of codes, will make the animation independent from its own position
+            transform.parent.position = transform.position; // Parent position, positioning it to our gameObject
+
+            // Local Postion of the child (because there's a parent child), we set it to zero (stay attached to it)
+            transform.localPosition = Vector3.zero;
+        }
+    }
+
+    IEnumerator FrogJump()
+    {
+        yield return new WaitForSeconds (Random.Range (1f, 4f));
+
+        animation_Started = true;
+        animation_Finished = false;
+
+        timesJumped++;
+
+        if (jumpLeft)
+        {
+            anim.Play ("FrogJumpLeft");
+        }
+        else
+        {
+            anim.Play ("FrogJumpRight");
+        }
+
+        StartCoroutine (coroutineName);
+    }
+
+    void AnimationFinished()
+    {
+        animation_Finished = true;
+
+        anim.Play ("FrogIdleLeft");
+
+        if (jumpLeft)
+        {
+            anim.Play ("FrogIdleLeft");
+        }
+        else
+        {
+            anim.Play ("FrogIdleRight");
+        }
+        if (timesJumped == 3)
+        {
+            timesJumped = 0;
+
+            Vector3 tempScale = transform.localScale;
+            tempScale.x *= -1f;
+            transform.localScale = tempScale;
+
+            jumpLeft = !jumpLeft;
+
+        }
+    }
+}
